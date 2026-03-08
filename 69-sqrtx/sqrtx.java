61c76925210cc0c0d0812bb370fc46a5f30c0cda
Pragati Parmar <pragatiparmar003@gmail.com>
2026-03-08 22:31:59 +0530

    Time: 1 ms (97.34%) | Memory: 42.5 MB (78.24%) - LeetSync

---
+++ 69-sqrtx/sqrtx.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int mySqrt(int x) {
+
+        if(x == 0 || x == 1){
+            return x;
+        }
+
+        int st = 1;
+        int end = x / 2;
+        int root = 0;
+
+        while(st <= end){
+
+            int mid = st + (end - st)/2;
+
+            if(mid <= x / mid){
+                root = mid;
+                st = mid + 1;
+            }else{
+                end = mid - 1;
+            }
+        }
+
+        return root;
+    }
+}
