ccda5f4c1d15d19c2800afe16e8f6c9f1d06bdb4
Pragati Parmar <pragatiparmar003@gmail.com>
2026-04-11 01:40:33 +0530

    Time: 67 ms (66.38%) | Memory: 78.3 MB (48.52%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -3,51 +3,68 @@ class Solution {
         int n = heights.length;
 
         Stack<Integer> st = new Stack<>();
+        int maxArea = 0;
 
-        int[] nse = new int[n];
-        int[] pse = new int[n];
+        for(int i = 0;i<=n;i++){
+            
+            int currHeight = (i == n) ? 0:heights[i];
 
-        //calculate nse
-        st.push(n-1);
-        nse[n-1] = n;
-        for(int i = n-2;i>=0;i--){
-            while(st.size()>0 && heights[st.peek()]>=heights[i]){
-                st.pop();
-            }
-            if(st.size() == 0){
-                nse[i] = n;
-            }else{
-                nse[i] = st.peek();
+            while(!st.isEmpty() && heights[st.peek()]>currHeight){
+                int height = heights[st.pop()];
+
+                int right = i;
+                int left = st.isEmpty() ? -1:st.peek();
+                int width = right-left-1;
+                maxArea = Math.max(maxArea , height*width);
             }
             st.push(i);
         }
+        return maxArea;
 
-         // emptying stack
-         while(st.size()>0) st.pop();
+        // int[] nse = new int[n];
+        // int[] pse = new int[n];
 
-        //calculate pse
-        st.push(0);
-        pse[0] = -1;
-        for(int i = 1;i<n;i++){
-            while(st.size()>0 && heights[st.peek()]>=heights[i]){
-                st.pop();
-            }
-            if(st.size() == 0){
-                pse[i] = -1;
-            }
-            else{
-                pse[i] = st.peek();
-            }
-            st.push(i);
-        }
+        // //calculate nse
+        // st.push(n-1);
+        // nse[n-1] = n;
+        // for(int i = n-2;i>=0;i--){
+        //     while(st.size()>0 && heights[st.peek()]>=heights[i]){
+        //         st.pop();
+        //     }
+        //     if(st.size() == 0){
+        //         nse[i] = n;
+        //     }else{
+        //         nse[i] = st.peek();
+        //     }
+        //     st.push(i);
+        // }
 
-        //max area of rec==
-        int max = -1;
-        for(int i = 0;i<n;i++){
-            int area = heights[i]*(nse[i]-pse[i] - 1);
-            max = Math.max(max,area);
-        }
-        return max;
+        //  // emptying stack
+        //  while(st.size()>0) st.pop();
+
+        // //calculate pse
+        // st.push(0);
+        // pse[0] = -1;
+        // for(int i = 1;i<n;i++){
+        //     while(st.size()>0 && heights[st.peek()]>=heights[i]){
+        //         st.pop();
+        //     }
+        //     if(st.size() == 0){
+        //         pse[i] = -1;
+        //     }
+        //     else{
+        //         pse[i] = st.peek();
+        //     }
+        //     st.push(i);
+        // }
+
+        // //max area of rec==
+        // int max = -1;
+        // for(int i = 0;i<n;i++){
+        //     int area = heights[i]*(nse[i]-pse[i] - 1);
+        //     max = Math.max(max,area);
+        // }
+        // return max;
 
     }
 }
