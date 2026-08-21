2bd92f364588a265306d53457dad5a145503a974
Pragati Parmar <pragatiparmar003@gmail.com>
2026-08-22 02:31:47 +0530

    Time: 5 ms (66.46%) | Memory: 47.4 MB (18.05%) - LeetSync

---
+++ 72-edit-distance/edit-distance.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int minDistance(String s1, String s2) {
+        int m = s1.length(), n = s2.length();
+        int[][] dp = new int[m+1][n+1];
+
+        // Empty s2 → delete all characters from s1
+        for (int i = 0; i <= m; i++) {
+            dp[i][0] = i;
+        }
+
+        // Empty s1 → insert all characters of s2
+        for (int j = 0; j <= n; j++) {
+            dp[0][j] = j;
+        }
+
+        for (int i = 1; i <= m; i++){
+            for (int j = 1; j <= n; j++) {
+                if(s1.charAt(i-1) == s2.charAt(j-1)) dp[i][j] = dp[i-1][j-1];
+                else{
+                    dp[i][j] = 1 + Math.min(dp[i-1][j-1], Math.min(dp[i-1][j], dp[i][j-1]));
+                }
+            }
+        }
+        return  dp[m][n];
+    }
+}
