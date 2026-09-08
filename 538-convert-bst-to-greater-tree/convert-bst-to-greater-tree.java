bc8ca4b293c03d3c8695114870fed819b2e24ac5
Pragati Parmar <pragatiparmar003@gmail.com>
2026-09-09 00:23:32 +0530

    Time: 0 ms (100.00%) | Memory: 47.6 MB (9.07%) - LeetSync

---
+++ 538-convert-bst-to-greater-tree/convert-bst-to-greater-tree.java
@@ -0,0 +1,15 @@
+class Solution {
+    int sum = 0;
+    public TreeNode convertBST(TreeNode root) {
+        sum = 0;
+        helper(root);
+        return root;
+    }
+    private void helper(TreeNode root){
+        if(root == null) return;
+        helper(root.right);
+        root.val = root.val + sum;
+        sum = root.val;
+        helper(root.left);
+    }
+}
